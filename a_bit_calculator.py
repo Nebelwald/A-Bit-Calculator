e8f51ab2681d71a1792b06ab38969f04ca015ca6
Leonhard Reichert <leonhard.klingert@outlook.com>
2021-07-11 17:49:36 +0200

    Edit input/output to be smoother and cleaner

--- a_bit_calculator.py
+++ a_bit_calculator.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python
 
-print("\n============================")
-print("== ABC | A BIT CALCULATOR ==")
-print("============================")
+import os
 
 while True:
+    format = ""
+
     # Get format
     while True:
-        format = input("\nUnit to convert from:\n[1] Bit\n[2] Byte\n[3] KByte\n[4] MByte\n[5] GByte\n[6] TByte\n>> ")
+        print("\n============================")
+        print("== ABC | A BIT CALCULATOR ==")
+        print("============================")
 
-        if not format.isdigit() or (int(format) < 1) or (int(format) > 6):
-            print("\n== Invalid input: Please enter a number between 1 and 6 ==")
+        print("\nUnit to convert from:\n[1] Bit\n[2] Byte\n[3] KByte\n[4] MByte\n[5] GByte\n[6] TByte\n>> ", end = "")
+
+        if (format == ""):
+            format = input()
         else:
+            print(format)
+
+        if not (not str(format).isdigit() or (int(format) < 1) or (int(format) > 6)):
             format = int(format)
-            break
 
     # Get value
-    while True:
-        try:
-            value = float(input("\nValue to convert: \n>> "))
-            break
-        except:
-            print("\n== Invalid input: Please enter a number ==")
+            try:
+                value = float(input("\nValue to convert: \n>> "))
+                break
+            except:
+                print("\n== Invalid input: Please enter a number ==")
+                input("[Enter]")
+                os.system('cls')
+        else:
+            print("\n== Invalid input: Please enter a number between 1 and 6 ==")
+            input("[Enter]")
+            format = ""
+            os.system('cls')
 
     # Calculate value to bit
     if format > 1:
@@ -38,6 +50,7 @@ while True:
 
     # Repeat the program
     repeat = input("\nDo you want to convert another number? [y/n]\n>> ")
+    os.system('cls')
 
     if repeat != "y":
         break
